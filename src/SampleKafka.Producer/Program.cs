299e81bf51f84735a7a08b60abed01510ce03183
jorgelucas <jorgelucasl91@gmail.com>
2022-09-03 17:03:14 -0300

    implementação do producer

--- src/SampleKafka.Producer/Program.cs
+++ src/SampleKafka.Producer/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Schemas.Avros;
+
+const string Topico = "cursos";
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using var producer = new ProducerBuilder<string, Curso>(config)
+    .SetValueSerializer(new AvroSerializer<Curso>(schemaRegistry))
+    .Build();
+
+var msg = new Message<string, Curso>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = new Curso
+    {
+        Id = Guid.NewGuid().ToString(),
+        Descricao = $"Curso de apache kafka {DateTime.Now}"
+    }
+};
+
+var result = await producer.ProduceAsync(Topico, msg);
+
+Console.WriteLine($"Offset: {result.Offset} \n Key:{result.Key} \n Partition:{result.Partition}");
