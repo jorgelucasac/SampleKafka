eed10c2467939b7051e1bf5034352398f52346cc
jorgelucas <jorgelucasl91@gmail.com>
2022-09-03 17:03:25 -0300

    implementação do consumer

--- src/SampleKafka.Consumer/Program.cs
+++ src/SampleKafka.Consumer/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Schemas.Avros;
+
+const string Topico = "cursos";
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ConsumerConfig
+{
+    GroupId = "devio",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, Curso>(config)
+    .SetValueDeserializer(new AvroDeserializer<Curso>(schemaRegistry).AsSyncOverAsync())
+    .Build();
+
+consumer.Subscribe(Topico);
+
+while (true)
+{
+    var result = consumer.Consume();
+
+    Console.WriteLine($"Key: {result.Message.Key}");
+    Console.WriteLine($"Id: {result.Message.Value.Id}");
+    Console.WriteLine($"Descricao: {result.Message.Value.Descricao}");
+}
