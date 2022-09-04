519d10a4a5afc088ecb238e21129d198e7651ef1
jorgelucas <jorgelucasl91@gmail.com>
2022-09-04 17:08:52 -0300

    Habilitando idempotência

--- src/SampleKafka.Advanced/ProducerTools.cs
+++ src/SampleKafka.Advanced/ProducerTools.cs
@@ -71,5 +71,50 @@ namespace SampleKafka.Advanced
         }
 
         #endregion Acks
+
+        #region Indepotencia
+
+        /// <summary>
+        /// produz mensagens de forma indepotente, impedindo mensagens duplicadas
+        /// </summary>
+        /// <param name="topico"></param>
+        /// <returns></returns>
+        public static async Task ProduzirIndepotente(string topico)
+        {
+            _count++;
+            var configuracao = new ProducerConfig
+            {
+                BootstrapServers = _bootstrapServers,
+                Partitioner = Partitioner.ConsistentRandom,
+
+                // propriedades para Habilitar idempotência
+                EnableIdempotence = true,
+                Acks = Acks.All,//todos os brokers devem receber uma cópia da msg
+                MaxInFlight = 1, // quantidade de conexões na sessão atual, (uma unica conexão publicando msg, mantendo a ordem de envio das msg)
+                MessageSendMaxRetries = 2, // maximo de retentativas de envio
+            };
+
+            try
+            {
+                using var producer = new ProducerBuilder<string, string>(configuracao).Build();
+                var mensagem = $"Mensagem ({_count}) - {Guid.NewGuid()}";
+
+                var result = await producer.ProduceAsync(topico, new Message<string, string>
+                {
+                    Key = Guid.NewGuid().ToString(),
+                    Value = mensagem
+                }); ;
+
+                Console.WriteLine($"<< enviada: \t {mensagem} - partição{result.Partition.Value}");
+
+                await Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+            }
+        }
+
+        #endregion Indepotencia
     }
 }
